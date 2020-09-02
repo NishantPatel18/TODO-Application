770c2dd69cc7d3fe6bef6423de7702298b367b27
Nishant Patel <nishantpatel1842@gmail.com>
2020-09-02 15:21:07 +1200

    Update App.js

--- mern-todo-app/src/App.js
+++ mern-todo-app/src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
         <div className="container">
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="https://google.com" target="_blank">
-              <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
+              <img src={logo} width="30" height="30" alt="Google.com" />
             </a>
             <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
             <div className="collpase navbar-collapse">
